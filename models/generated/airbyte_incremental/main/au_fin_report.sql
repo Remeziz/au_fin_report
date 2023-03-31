a3888e28925017114c82ee95dd552ee3c65b1178
Remeziz <79668248+Remeziz@users.noreply.github.com>
2023-03-31 13:19:15 +0300

    Update au_fin_report.sql

--- models/generated/airbyte_incremental/main/au_fin_report.sql
+++ models/generated/airbyte_incremental/main/au_fin_report.sql
@@ -1,4 +1,9 @@
-{{ config(schema='main') }}
+{{ confg(   
+   unique_key = '_airbyte_ab_id',
+   schema = "main",
+   materialized = 'table',
+   tags = [ "top-level" ]
+) }}
 select date_time,
        settlement_id,
        type,
