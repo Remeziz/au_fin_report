62680236178f142ad8a7f7c8df724b459feb2340
analytics-easyfeet <129346352+analytics-easyfeet@users.noreply.github.com>
2023-04-03 12:23:26 +0300

    Update au_fin_report.sql

--- models/generated/airbyte_incremental/main/au_fin_report.sql
+++ models/generated/airbyte_incremental/main/au_fin_report.sql
@@ -25,7 +25,7 @@ select date_time,
 
 from (
          select
-                `posted-date`  as date_time,
+               DATE_FORMAT(`posted-date`, '%Y-%m-%d %H:%i:%s.%f')  as date_time,
                 `settlement-id`                                              as settlement_id,
                 `transaction-type`                                           as type,
                 `order-id`                                                   as order_id,
